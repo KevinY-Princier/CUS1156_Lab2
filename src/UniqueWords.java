296d223baa21dd3a1c4a2b7864f5a5d93b062009
KevinYameogo <yameokevin234@gmail.com>
2025-02-15 20:19:31 -0500

    finished lab2

--- src/UniqueWords.java
+++ src/UniqueWords.java
@@ -12,10 +12,21 @@ public class UniqueWords
 	  int count = 0;
 	  
       for (int i = 0; i < list.size(); i++)
-      {		 for (int j = 0; j < list.size(); j++)
-		 {
-			
+      {	  //
+    	  boolean isUnique = true;
+    	  for (int j = 0; j < i; j++)
+		 {	  // if word appears more than once
+    		  if (list.get(i).equals(list.get(j)))  
+              {
+                 isUnique = false;  
+                 break;  
+              }
 		 }
+    	  
+    	  if (isUnique)
+          {
+             count++;
+          }
       }
 	  return count;
    }
@@ -30,6 +41,7 @@ public class UniqueWords
 	  words.add("orange");
 	  words.add("sun");
 	  words.add("moon");
+	  words.add("sun");
    
 	  int unique = countUnique(words);
       System.out.println(words + " has " + unique + " unique words");
